388be9fd3398757264ee7357746681b6d7bc9dab
Vanii <valcantaram@miumg.edu.gt>
2023-05-27 15:18:42 -0600

    UPDATE: Bubble Sort

--- App.java
+++ App.java
@@ -1,34 +1,37 @@
 
-
-public class App{
+public class App {
     public static void main(String[] args) {
 
-        //Declaracion de matriz
+        // Declaracion de matriz
         int[] vector = new int[9];
         int aux = 0;
 
-        //Llenar la matriz con numeros aleatorios
-        for(int i = 0; i < vector.length; i++){
-            vector[i] = (int) (Math.random()*50);
+        // Llenar la matriz con numeros aleatorios
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = (int) (Math.random() * 50);
         }
 
-        //Mostrar matriz de numeros desordenados
+        // Mostrar matriz de numeros desordenados
         System.out.println("Arreglo desordenado:");
-        for(int i = 0; i < vector.length; i++){
+        for (int i = 0; i < vector.length; i++) {
             System.out.print(vector[i] + " ");
         }
 
-        //Ordenamiento de burbuja
-        for(int i = 0; i < vector.length; i++){
-            if(vector[i] < vector[i+1]){
-                aux = vector[i];
-                vector[i] = vector[i+1];
+        // Ordenamiento de burbuja
+        for (int j = 1; j < vector.length; j++) {
+            for (int i = 1; i < vector.length; i++) {
+
+                if (vector[i - 1] < vector[i]) {
+                    aux = vector[i - 1];
+                    vector[i - 1] = vector[i];
+                    vector[i] = aux;
+                }
             }
         }
 
-        //Mostrar vector ordenado
-        System.out.println("Arreglo ordenado:");
-        for(int i = 0; i < vector.length; i++){
+        // Mostrar vector ordenado
+        System.out.println("\n\tArreglo ordenado:");
+        for (int i = 0; i < vector.length; i++) {
             System.out.print(vector[i] + " ");
         }
     }
